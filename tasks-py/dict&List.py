4ebaefcc1eff37bc26ba03a0d9097727f4e25715
DueWeb <keviin.sundberg@gmail.com>
2022-10-20 10:07:11 +0200

    idk

---
+++ tasks-py/dict&List.py
@@ -0,0 +1,8 @@
+def question(question, answer):
+    answer = input(answer)
+def  input_how_much_you_like_things(verb, value):
+    thing_you_like[verb] = value
+question = ("gillar du laga mat?")
+print("mycket, sådär, inte")
+input(answer)
+thing_you_like = {}
